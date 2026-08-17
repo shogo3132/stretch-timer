7f5967a4f15728eb01719438d28d327a04cd44b9
shogo3132 <64763957+shogo3132@users.noreply.github.com>
2026-08-17 11:00:32 +0900

    Fix stale app build detection for v47

--- ux-v22.js
+++ ux-v22.js
@@ -1,5 +1,5 @@
 (function(){
-  var CURRENT_BUILD='stretch-timer-v30';
+  var CURRENT_BUILD='stretch-timer-v47';
   var BUILD_KEY='stretchTimer.appBuild';
   var PENDING_NOTICE='stretchTimer.pendingNotice';
   var refreshRunning=false;
